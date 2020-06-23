5faa5aab3edc8a4e01af2635608965ecd2c25088
rmiri <r.miriuk@tutanota.com>
2020-06-23 20:38:57 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.delete_all
+
+3.times do |i|
+Question.create(
+    content: "#{i+1} What's the meaning of the universe?",
+    info: "In difficult times you must know one thing one",
+    answer: "42",
+    category:'cultural',
+    
+)
+end
+3.times do |i|
+Question.create(
+    content: "#{i+1} What's the meaning of the universe?",
+    info: "In difficult times you must know one thing one",
+    answer: "42",
+    category:'trivia',
+    language: 'javascript'
+)
+end
+4.times do |i|
+Question.create(
+    content: "#{i+1} What's the meaning of the universe?",
+    info: "In difficult times you must know one thing one",
+    answer: "42",
+    category: 'coding',
+    language: 'ruby'
+)
+end
